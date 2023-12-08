fa47f52bef363b0f112f053ff673c17da029a140
Nithish-Lakshmanan <nithish@agentz.ai>
2023-12-08 22:50:08 +0530

    feat(NLP): added new concept of similarity matching between words

---
+++ 03/main.js
@@ -0,0 +1,7 @@
+// TODO Implement or try Levenshtein Distance function to find misspelled words using similarity matching
+/* 1. Construct a predefined set of words dataset
+ * 2. Take user input string
+ * 3. Compare the given input string with all the words in the dataset and find the similarity score
+ * 4. Set a threshold and see if the similarity score exceeds it
+ * 5. If it exceeds then return that word, else no match found
+ */
