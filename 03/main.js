fc5c61aeb255b11987e1c4a6c821da5d386f547d
Nithish-Lakshmanan <nithish@agentz.ai>
2024-01-10 21:07:22 +0530

    feat: added simple SQS implementation to grasp the conecpt

--- 03/main.js
+++ 03/main.js
@@ -5,3 +5,33 @@
  * 4. Set a threshold and see if the similarity score exceeds it
  * 5. If it exceeds then return that word, else no match found
  */
+
+// SQS Basic simulation
+let messageQueue = [];
+const sendMessage = (msg) => {
+    messageQueue.push({ ...msg, visibility: true });
+};
+const receiveMessage = (consumerId) => {
+    const fifoMsgIndex = messageQueue.findIndex((msg) => msg.visibility);
+    if (fifoMsgIndex !== -1) {
+        messageQueue[fifoMsgIndex].visibility = false;
+        return messageQueue[fifoMsgIndex].message;
+    }
+    return null;
+};
+const pollMessage = (consumerId) => {
+    setInterval(() => {
+        const message = receiveMessage(consumerId);
+        if (message) {
+            return message;
+        } else {
+            return null;
+        }
+    }, 2000);
+};
+sendMessage({ id: 1, message: "Im first message", metaData: {} });
+sendMessage({ id: 2, message: "Im second message", metaData: {} });
+sendMessage({ id: 3, message: "Im third message", metaData: {} });
+
+console.log(pollMessage(1));
+console.log(pollMessage(2));
